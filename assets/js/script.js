9fbe3675e941f4d626679202af9c0a43a24b666a
Xiaojing <8redfishcreative@gmail.com>
2022-09-21 10:00:45 -0400

    update searched list

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ var searchCityButton = document.querySelector('.search-btn');
 var APIKey = "adf46be5146fd6c70576939f90013837";
 var cityItemEl = document.querySelector('#city-list');
 
-var cityName = localStorage.getItem('cities');
+var cityName = localStorage.getItem("city");
  var clearEl = document.querySelector('.clear-history');
 
-  let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+  var searchHistoryList = [];
 
 var URLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + '&units=imperial' +APIKey;
 //clicking search button will trigger sarchApi 
@@ -15,77 +15,55 @@ var URLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityNa
 function handleSubmit(event){
     event.preventDefault();
 
-     var searchValue = searchCity.value.trim();
-    if(!searchValue){
+     var city = searchCity.value.trim();
+    if(!city){
     console.error('You need a search input value!');
     return;
     }
-    getWeather(searchValue);
+    getWeather(city);
     searchCity.value="";
-    searchHistory.push(searchValue);
-    saveCityDataToLocal(searchValue);
-    displayCities(searchValue);
+    
+  
+    displayCities(city);
 }
 searchForm.addEventListener('submit', handleSubmit);
 
-// Function to save the city to localStorage
-// Sets the input value in localStorage
-function saveCityDataToLocal(newCity) {
-    searchValue = searchCity.value.trim()
-    localStorage.setItem('cityNameStore', searchValue);
-  let cityExists = false;
-    // Check if City exists in local storage
-    for (let i = 0; i < localStorage.length; i++) {
-        if (localStorage["cities" + i] === newCity) {
-            cityExists = true;
-            break;
-        }
-    }
-    // Save to localStorage if city is new
-    if (cityExists === false) {
-        localStorage.setItem('cities' + localStorage.length, newCity);
-    }
-}
-
-
-
-
-//get user search and inset to ul
+//get user search save to local and inset to ul 
 
 function displayCities(){
-if(localStorage.length===0){
-return;
-
-}else{
-// Append the search input from localStorage to the cities list
-console.log(localStorage);
-for(let i=0;i<localStorage.length;i++){
- let city = localStorage.getItem("cities" + i);
-var cityList = document.createElement("input");
-    cityList.setAttribute("type","text");
-    cityList.setAttribute("readonly",true);
-    cityList.setAttribute("class", "d-block bg-white city-li form-control");
-    cityList.setAttribute("style", "width: 100%");
-    cityList.setAttribute("value", city);
-    cityList.addEventListener("click",function() {
-                getWeather(cityList.value);
+var city = searchCity.value.trim();
+ searchHistoryList.push(city);
+  localStorage.setItem("city", JSON.stringify(searchHistoryList));
+    console.log(searchHistoryList); 
+   
+cityItemEl.innerHTML="";
+for(let i=0; i<searchHistoryList.length; i++){
+var searchedCity = document.createElement("input");
+    searchedCity.setAttribute("type","text");
+    searchedCity.setAttribute("readonly",true);
+    searchedCity.setAttribute("class", "d-block bg-white city-li form-control");
+    searchedCity.setAttribute("style", "width: 100%");
+    searchedCity.setAttribute("value", searchHistoryList[i]);
+    searchedCity.addEventListener("click",function() {
+                getWeather(searchedCity.value);
             })
-    cityItemEl.appendChild(cityList);
 
+    cityItemEl.appendChild(searchedCity);
+  
 }
+}
+ 
 
-}
-
-}
 
   // Clear History button
     clearEl.addEventListener("click", function () {
         localStorage.clear();
-        searchHistory = [];
+        searchHistoryList = [];
         displayCities();
     })
 
 
+// Request Open Weather API based on user input
 // Request Open Weather API based on user input
 function getWeather(searchValue){
  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchValue+ '&appid=' + APIKey +'&cnt=5') 
@@ -99,3 +77,4 @@ function getWeather(searchValue){
     })
 
 }
+console.log(getWeather("cleveland"));
